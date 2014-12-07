6a93f94d69790dc834f416d63fbf86ed64cad8e1
kirimaks <kirimaks@yahoo.com>
2014-12-07 17:57:51 +0300

    Add cpu frequency control.

--- main.c
+++ main.c
@@ -7,43 +7,6 @@
 #include"temp_alarm.h"
 #include<stdint.h>
 
-
-int main(int argc, char **argv)
-{
-    FILE *f = NULL;
-    char buff[BUFF_SIZE];
-    char cmd[100] = "sudo -u ";
-
-    strcat(cmd, MY_USER);
-    strcat(cmd, " DISPLAY=");
-    strcat(cmd, DISP_NUM);
-    strcat(cmd, " ");
-    strcat(cmd, "notify-send ");
-    strcat(cmd, APP_NAME);
-    strcat(cmd, " ");
-    strcat(cmd, SHOW_TEXT);
-
-    check_args(argc,argv,&f);
-
-    int8_t cur_temp = 0;
-
-    for(;;) {
-        fread(buff, sizeof(char), LENGTH_TO_READ, f);
-        fseek(f,0,SEEK_SET);
-        fflush(f);
-
-        if((cur_temp = check_temp(buff))) {
-            strncat(cmd, buff,2);
-            strcat(cmd, "\"\0");
-            system(cmd);
-            cmd[strlen(cmd)-3] = '\0';
-        }
-        sleep(INTERVALS);
-    }
-
-    return 0;
-}
-
 void check_args(int argc, char **argv, FILE **f) 
 {   /* To do a lot. */
     if(argc == 1) {
@@ -55,11 +18,69 @@ void check_args(int argc, char **argv, FILE **f)
     }
 }
 
-int8_t check_temp(char *str_temp) 
+int8_t check_temp(char *str_temp, char *cur_mode) 
 {   /* To do a lot. */
-    int temp = atoi(str_temp);
-    /*printf("{%d}\n", temp);*/
+    int cur_temp = atoi(str_temp);
 
-    /* Maybe set cpu to powersave mode from this function if the temperature is too high. */
-    return (temp >= THRESHOLD) ? temp : 0;
+    if (cur_temp >= THRESHOLD) {    /* Go to powersave mode. */
+        /*printf("High temperature.\n");*/
+        if (strncmp(cur_mode, "powersave", 9)) {
+            /*printf("*** Write powersave mode ***\n");*/
+            system("for c in $(ls -d /sys/devices/system/cpu/cpu[0-9]*); do echo powersave >$c/cpufreq/scaling_governor; done");
+        }
+    } else if (cur_temp <= 65) {    /* Go to ondemand mode */
+        /*printf("Low temperature.\n");*/
+        if (strncmp(cur_mode, "ondemand", 8)) {
+            /*printf("*** Write ondemand mode ***\n");*/
+            system("for c in $(ls -d /sys/devices/system/cpu/cpu[0-9]*); do echo ondemand >$c/cpufreq/scaling_governor; done");
+        }
+    }
+
+    /* Should return not zero for make notify send. */
+    return (cur_temp >= THRESHOLD) ? cur_temp : 0;
+}
+
+int main(int argc, char **argv)
+{
+    FILE *f = NULL;
+    /* Open file with current cpu mode. */
+    FILE *mode_file = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "r");
+    char buff[BUFF_SIZE];
+    char cmd[100] = "sudo -u ";
+
+/* Create sting with arguments. (notify) */
+    strcat(cmd, MY_USER);
+    strcat(cmd, " DISPLAY=");
+    strcat(cmd, DISP_NUM);
+    strcat(cmd, " ");
+    strcat(cmd, "notify-send ");
+    strcat(cmd, APP_NAME);
+    strcat(cmd, " "); strcat(cmd, SHOW_TEXT); 
+    check_args(argc,argv,&f);
+
+    int8_t cur_temp = 0;
+    char cur_mode[BUFF_SIZE];
+
+
+    for(;;) {
+        /* Read from temp file. */
+        fread(buff, sizeof(char), 2, f);    /* Read 2 symbols. */  
+        fseek(f, 0L, SEEK_SET);
+        fflush(f);
+
+        /* Read from file wiht mode. */
+        fread(cur_mode, sizeof(char), 9, mode_file);
+        fseek(mode_file, 0L, SEEK_SET);
+        fflush(mode_file);
+
+        if((cur_temp = check_temp(buff, cur_mode))) { /* Create number of temperature in this call.   */
+            strncat(cmd, buff,2);           /* Add string with current temperature.         */
+            strcat(cmd, "\"\0");
+            system(cmd);
+            cmd[strlen(cmd)-3] = '\0';
+        }
+        sleep(INTERVALS);
+    }
+
+    return 0;
 }
